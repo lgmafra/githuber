02f7e414b7c79b6125c686dfc95bdf8d1bc19ed1
Luiz Gustavo Sampaio Mafra de Santana <lgmafra@gmail.com>
2019-05-11 23:40:06 -0300

    customize the repositories page

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, ActivityIndicator, FlatList } from 'react-native';
 import Header from '~/components/Header';
 import PropTypes from 'prop-types';
-
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '~/services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositório" />
-  </View>
-);
+import RepositoryItem from './RepositoryItem';
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
@@ -17,8 +15,56 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Repositories.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
+
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadRepositories}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Repositórios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
 
 export default Repositories;
