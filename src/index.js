3550260d90ab18c078600eeaab849bbe85253b85
Luiz Gustavo Sampaio Mafra de Santana <lgmafra@gmail.com>
2019-05-11 21:09:20 -0300

    defined intial route in app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import '~/config/ReactotroConfig';
 
-import Routes from '~/routes';
+import createNavigator from '~/routes';
 
-const App = () => <Routes />;
+class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+
+    return <Routes />;
+  }
+}
 
 export default App;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import AsyncStorage from '@react-native-community/async-storage';
 import { View, Text } from 'react-native';
+
+AsyncStorage.clear();
 
 const Repositories = () => (
   <View>
